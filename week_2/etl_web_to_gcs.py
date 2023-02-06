3b0d2bc3504217e87f270e7a195b5436f3ab9a07
Adam <adamsaparudin@gmail.com>
2023-02-06 23:02:43 +0700

    update code to allow save parqueet anywhere

--- week_2/etl_web_to_gcs.py
+++ week_2/etl_web_to_gcs.py
@@ -15,8 +15,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df: pd.DataFrame):
     """Clean dtype"""
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+    df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
     print(df.head(2))
     print(df.dtypes)
     print(f"rows: {len(df)}")
@@ -26,7 +26,7 @@ def clean(df: pd.DataFrame):
 @task(log_prints=True)
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out as parquet file"""
-    path = Path(f"data/{color}/{dataset_file}.parquet")
+    path = Path(f"./{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
 
